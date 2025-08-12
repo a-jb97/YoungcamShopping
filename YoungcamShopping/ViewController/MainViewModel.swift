9954f084161b577776cdaba0d83fb103fc762132
a-jb97 <a_jb97@naver.com>
2025-08-12 17:48:46 +0900

    [Feat] Observable 클래스 구현

---
+++ YoungcamShopping/ViewController/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  YoungcamShopping
+//
+//  Created by 전민돌 on 8/12/25.
+//
+
+import Foundation
---
+++ YoungcamShopping/ViewController/SearchResultViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultViewModel.swift
+//  YoungcamShopping
+//
+//  Created by 전민돌 on 8/12/25.
+//
+
+import Foundation
